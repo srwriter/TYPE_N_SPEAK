be7a97dba94aa8134957796d7e835b2d0f3fca5e
srwriter <s.mrobinson@hotmail.com>
2021-12-02 19:55:18 +0300

    Added git.ignore json files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+// Init SpeechSynth API
+const synth = window.speechSynthesis;
+
+// DOM Elements
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#rate-value');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');
+const body = document.querySelector('body');
+
 //Browser identifier
 // Firefox 1.0+
 var isFirefox = typeof InstallTrigger !== 'undefined';
